3df1b2764f692629eb657f23dfe156fbbe4bce88
rydz <ridomahgans@gmail.com>
2023-06-07 14:57:29 +0700

    ewe

---
+++ plugins/info-sc.js
@@ -0,0 +1,10 @@
+let handler = async (m, { conn, args, command }) => {
+  let message = 'beli lah monyet'
+  m.reply(message) //jangan hapus om link yt gw nya kalau mau tambahin aja ya ganteng/cantik/om cuakss 🗿
+}
+
+handler.help = ['sc']
+handler.tags = ['info']
+handler.command = ['sc', 'sourlcode']
+
+export default handler
